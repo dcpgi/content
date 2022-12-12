bcfb51c77c1dcadf1f2f0f4b0014dc99246caa3a
Slemmie <thorvejeneriksen@gmail.com>
2022-12-12 07:27:06 +0100

    restructure build script

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
+echo "building catalog..."
 cp -r ./src/general/*.md ../catalog/src/
 cp ./src/general/SUMMARY_catalog.md ../catalog/src/SUMMARY.md
+(cd ../catalog/ && mdbook build)
 
+echo "building guide..."
 cp -r ./src/general/*.md ../guide/src/
 cp ./src/general/SUMMARY_guide.md ../guide/src/SUMMARY.md
+(cd ../guide/ && mdbook build)
 
+echo "building welcome..."
 cp -r ./src/welcome/*.md ../welcome/src/
 cp ./src/welcome/SUMMARY.md ../welcome/src/SUMMARY.md
-
-(cd ../catalog/ && mdbook build)
-(cd ../guide/ && mdbook build)
 (cd ../welcome/ && mdbook build)
 
